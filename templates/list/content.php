ba554745be86d92301d837ebaadd617ce4d7175c
MrKapowski <35972784+MrKapowski@users.noreply.github.com>
2019-05-30 21:03:32 +0100

    wondering why I'm getting post_kind errors

--- templates/list/content.php
+++ templates/list/content.php
@@ -1,3 +1,3 @@
 <article>
-    <?= ( function_exists( 'has_post_kind' ) && has_post_kind() ) ? get_post_kind() : get_post_format(); ?>
+    <?= get_post_kind(); ?> <?= get_post_format(); ?>
 </article>

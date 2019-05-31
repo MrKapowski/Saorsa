a503baa79fd20deed992cdede3874a305ac03af8
MrKapowski <35972784+MrKapowski@users.noreply.github.com>
2019-05-31 14:13:30 +0100

    trying some things with custom logos

--- templates/posts/publisher-details.php
+++ templates/posts/publisher-details.php
@@ -3,17 +3,15 @@
 	<meta itemprop="url" content="<?php bloginfo( 'url' ); ?>">
 	<meta itemprop="name" content="<?php bloginfo( 'name' ); ?>">
 	<?php
-		if ( has_custom_logo() ) {
-			$custom_logo_id = get_theme_mod( 'custom_logo' );
-			$image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-			print_r($image);
-			//echo $image[0];
-		}
+	if ( has_custom_logo() ) :
+		$custom_logo_id = get_theme_mod( 'custom_logo' );
+		$image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 	?>
 	<span itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
-		<meta itemprop="url" content="<?php echo esc_attr( get_theme_file_uri( 'assets/img/kapow_magenta.png' ) ); ?>">
-		<meta itemprop="height" content="295px">
-		<meta itemprop="width" content="300px">
+		<meta itemprop="url" content="<?= $image[0]; ?>">
+		<meta itemprop="width" content="<?= $image[1]; ?>px">
+		<meta itemprop="height" content="<?= $image[2]; ?>px">
 	</span>
+	<?php endif; ?>
 </span>
 <?php

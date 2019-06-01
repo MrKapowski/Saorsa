3c3e53aeff6cc27976388fb6e054ff3a54a8b6ca
MrKapowski <35972784+MrKapowski@users.noreply.github.com>
2019-06-01 14:06:00 +0100

    testing better title tags

--- integrations/post-kinds.php
+++ integrations/post-kinds.php
@@ -66,9 +66,9 @@ if(defined('THE_SEO_FRAMEWORK_VERSION')){
 
 function saorsa_kind_title( $title, $args = '' ) {
     $post = get_queried_object();
-    $title = empty($title) ? 'Untitled' : $title;
+    $in_title = empty($title) ? 'Untitled' : $title;
     $mod_title = array();
-    if ($title === 'Untitled' && is_single() ) {
+    if ($in_title === 'Untitled' && is_single() ) {
         $mf2_post = new MF2_Post( $post );
         $kind     = $mf2_post->get( 'kind', true );
         $singular = Kind_Taxonomy::get_kind_info($kind, 'singular_name');

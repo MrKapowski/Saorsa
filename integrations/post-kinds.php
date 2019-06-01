24ad4578cb5d2232b27e5bf588d7dbca82824821
MrKapowski <35972784+MrKapowski@users.noreply.github.com>
2019-06-01 13:44:41 +0100

    testing better title tags

--- integrations/post-kinds.php
+++ integrations/post-kinds.php
@@ -66,6 +66,7 @@ if(defined('THE_SEO_FRAMEWORK_VERSION')){
 
 function saorsa_kind_title( $title ) {
     $post = get_queried_object();
+    $title = $title ?? 'Untitled';
     if ($title === 'Untitled' && is_single() ) {
         $mf2_post = new MF2_Post( $post );
         $kind     = $mf2_post->get( 'kind', true );

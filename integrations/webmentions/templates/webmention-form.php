795f193aa70e4c4a33dfb328b1fb600ac869088c
MrKapowski <35972784+MrKapowski@users.noreply.github.com>
2019-06-21 16:06:19 +0100

    remove unnecessary small tag

--- integrations/webmentions/templates/webmention-form.php
+++ integrations/webmentions/templates/webmention-form.php
@@ -1,7 +1,7 @@
 <section class="comment--webmention">
 	<h2 class="comment--webmention--title"><?php esc_html_e( 'Webmention', 'webmention' ); ?></h2>
 	<p class="comment--webmention--intro">
-<small><?php echo get_webmention_form_text( get_the_ID() /*phpcs:ignore*/); ?></small>
+		<?php echo get_webmention_form_text( get_the_ID() /*phpcs:ignore*/); ?>
 	</p>
 	<form id="webmention-form" class="comment--webmention--form" action="<?php echo esc_url( get_webmention_endpoint() ); ?>" method="post">
 		<input id="webmention-source" type="url" name="source" class="" placeholder="<?php esc_attr_e( 'URL/Permalink of your article', 'webmention' ); ?>" />

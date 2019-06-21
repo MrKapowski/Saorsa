52c21e6c50344b887b4e038ed8cb1283176b89a4
MrKapowski <35972784+MrKapowski@users.noreply.github.com>
2019-06-21 09:31:57 +0100

    moving stuff around

--- comments.php
+++ comments.php
@@ -16,39 +16,42 @@ if ( post_password_required() ) {
 }
 ?>
 
-	<header>
-		<h4 class="comments-title">
-			<?php
-			printf(
-				// translators:
-				esc_html( _nx( '%s Responses', '%s Responses', get_comments_number(), 'comments title', 'saorsa' ) ),
-				esc_html( number_format_i18n( get_comments_number() ) ),
-				'<span>' . esc_html( get_the_title() ) . '</span>'
-			);
-			?>
-		</h4>
-	</header>
-	<section>
-		<?php if ( have_comments() ) : ?>
-		<ol class="comment-list">
-			<?php
-				wp_list_comments(
-					array(
-						'type' => 'comment',
-					)
-				);
-			?>
-		</ol><!-- .comment-list -->
-			<?php do_action( 'comment_mentions' ); ?>
-		<hr>
-		<?php endif; ?>
-		<?php if ( ! comments_open() && get_comments_number() ) : ?>
-		<p class="no-comments"><?php esc_html_e( 'Responses are closed.', 'saorsa' ); ?></p>
-		<?php endif; ?>
-	</section>
-	<footer>
-	<?php // if ( comments_open() ) : ?>
-		<div id="commentform-top"></div>
-		<?php comment_form( saorsa_comment_form_args() ); ?>
-	<?php // endif; ?>
-	</footer>
+				<header>
+					<h4 class="comments-title">
+						<?php
+						printf(
+							// translators:
+							esc_html( _nx( '%s Responses', '%s Responses', get_comments_number(), 'comments title', 'saorsa' ) ),
+							esc_html( number_format_i18n( get_comments_number() ) ),
+							'<span>' . esc_html( get_the_title() ) . '</span>'
+						);
+						?>
+					</h4>
+				</header>
+				<section class="comments">
+					<h5>Comments</h5>
+					<?php if ( have_comments() ) : ?>
+					<ol class="comment-list">
+						<?php
+							wp_list_comments(
+								array(
+									'type' => 'comment',
+								)
+							);
+						?>
+					</ol><!-- .comment-list -->
+				</section>
+				<?php do_action( 'comment_mentions' ); ?>
+				<?php endif; ?>
+				<?php if ( ! comments_open() && get_comments_number() ) : ?>
+				<section class="comments">
+					<p class="no-comments"><?php esc_html_e( 'Responses are closed.', 'saorsa' ); ?></p>
+				</section>
+				<?php endif; ?>
+				
+				<footer>
+				<?php // if ( comments_open() ) : ?>
+					<div id="commentform-top"></div>
+					<?php comment_form( saorsa_comment_form_args() ); ?>
+				<?php // endif; ?>
+				</footer>

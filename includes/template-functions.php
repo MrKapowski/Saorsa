600e39893d392b320363e0de88a5844df6c7a087
MrKapowski <35972784+MrKapowski@users.noreply.github.com>
2019-06-21 08:38:26 +0100

    Adding missing comment functions

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -179,3 +179,131 @@ if ( !function_exists( 'saorsa_deregister_default_styles' ) ) {
  * Removes the deregistered Jetpack stylesheets
  */
 add_action( 'wp_print_styles', 'saorsa_deregister_default_styles', 100 );
+
+
+/**
+ * Registers the custom walker for this theme
+ * @since K 0.8.4
+ */
+if ( ! function_exists( 'saorsa_filter_comment_args' ) ) {
+	function saorsa_filter_comment_args( $args ) {
+		$args['walker'] = new Saorsa_Walker_Comment();
+		return $args;
+	}
+}
+/**
+ * Returns the number of webmentions, pings/trackbacks the current post has.
+ * Originally found in functions.php
+ * @since 0.3
+ */
+if ( ! function_exists( 'saorsa_comment_count_mentions' ) ) {
+	function saorsa_comment_count_mentions() {
+		$args   = array(
+			'post_id'  => get_the_ID(),
+			'type__in' => array( 'pings', 'webmention' ),
+		);
+		$_query = new WP_Comment_Query();
+		return count( $_query->query( $args ) );
+	}
+}
+
+/**
+ * Formats the comment form into markup compatible with the K theme.
+ */
+if ( ! function_exists( 'saorsa_comment_form_args' ) ) {
+	function saorsa_comment_form_args() {
+		if ( ! is_user_logged_in() ) {
+			$comment_notes_before = '';
+			$comment_notes_after  = '';
+		} else {
+			$comment_notes_before = '';
+			$comment_notes_after  = '';
+		}
+
+		$user          = wp_get_current_user();
+		$commenter     = wp_get_current_commenter();
+		$req           = get_option( 'require_name_email' );
+		$aria_req      = ( $req ? " aria-required='true'" : '' );
+		$consent       = empty( $commenter['comment_author_email'] ) ? '' : ' checked="checked"';
+		$login_link    = sprintf(// translators:
+			__( 'You must be <a href="%s">logged in</a> to post a comment.', 'saorsa' ),
+			wp_login_url( apply_filters( 'the_permalink', get_permalink() ) )
+		);
+		$loggedin_link = sprintf(// translators:
+			__( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>', 'saorsa' ),
+			admin_url( 'profile.php' ),
+			$user->display_name,
+			wp_logout_url( apply_filters( 'the_permalink', get_permalink() ) )
+		);
+
+		$args = array(
+			'id_form'              => 'commentform',
+			'id_submit'            => 'submit',
+			'title_reply'          => 'Leave a comment',
+			// translators:
+			'title_reply_to'       => __( 'Leave a Reply for %s', 'saorsa' ),
+			'cancel_reply_link'    => __( 'Cancel Reply', 'saorsa' ),
+			'label_submit'         => __( 'Submit Comment', 'saorsa' ),
+			'must_log_in'          => '<p class="form-text must-log-in text-muted">' . $login_link . '</p>',
+			'logged_in_as'         => '<p class="form-text text-muted logged-in-as">' . $loggedin_link . '</p>',
+			'comment_notes_before' => $comment_notes_before,
+			'comment_notes_after'  => $comment_notes_after,
+			'fields'               => apply_filters(
+				'comment_form_default_fields',
+				array(
+					'author'  =>
+						'<div class="form-row"><div class="comment-form-author form-group col-md-4"><label for="author" class="sr-only">' . __( 'Name', 'saorsa' ) . '</label>' . ( $req ? '' : '' ) .
+						'<input id="author" class="form-control" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+						'"' . $aria_req . ' placeholder=' . __( 'Name', 'saorsa' ) . '></div>',
+					'email'   =>
+						'<div class="comment-form-email form-group col-md-4"><label for="email" class="sr-only">' . __( 'Email', 'saorsa' ) . '</label>' . ( $req ? '' : '' ) .
+						'<input id="email" class="form-control" name="email" type="text" value="' . esc_attr( $commenter['comment_author_email'] ) .
+						'"' . $aria_req . ' placeholder=' . __( 'Email', 'saorsa' ) . '></div>',
+					'url'     =>
+						'<div class="comment-form-url form-group col-md-4"><label for="url" class="sr-only">' . __( 'Website', 'saorsa' ) . '</label>' .
+						'<input id="url" class="form-control" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+						'" placeholder=' . __( 'Website', 'saorsa' ) . '></div></div>',
+					'cookies' => '<div class="comment-form-consent form-group form-check col-md-12"><input class="form-check-input" id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" type="checkbox" value="yes"' . $consent . ' />' .
+					'<label for="wp-comment-cookies-consent" class="form-check-label">' . __( 'Save my name, email, and website in this browser for the next time I comment.', 'saorsa' ) . '</label></div>',
+				)
+			),
+		);
+
+		return $args;
+	}
+}
+/**
+ * Recreates the comment form textarea HTML for reinclusion in comment form
+ */
+if ( ! function_exists( 'saorsa_add_textarea' ) ) {
+	function saorsa_add_textarea() {
+		$arg['comment_field'] = '<div class="form-row"><div class="form-group col-md-12 comment-form-comment"><label for="comment">' . __( 'Comment', 'saorsa' ) . '</label>' .
+		'<textarea class="form-control" id="comment" name="comment" cols="60" rows="6" aria-required="true"></textarea></div></div>';
+		return $arg;
+	}
+}
+
+/**
+ * Adds additional classes to the submit button on the comment form
+ */
+if ( ! function_exists( 'saorsa_submit_button' ) ) {
+	function saorsa_submit_button( $submit_field ) {
+		$changed_submit = str_replace( 'name="submit" type="submit" id="submit"', 'name="submit" type="submit" id="submit" class="btn btn-primary"', $submit_field );
+		return $changed_submit;
+	}
+}
+/**
+ * Filters wp_list_comments $args to apply our Walker_Comment
+ * @since K 0.8.4
+ */
+add_filter( 'wp_list_comments_args', 'saorsa_filter_comment_args' );
+
+/**
+ * Adds the reformatted textarea into the comment form
+ */
+add_action( 'comment_form_defaults', 'saorsa_add_textarea' );
+
+/**
+ * Filters the comment form, to add our customised submit button
+ */
+add_filter( 'comment_form_submit_field', 'saorsa_submit_button' );

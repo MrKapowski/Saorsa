04e36d9ea8eb93164dd6f73630f2f12493c3074c
MrKapowski <35972784+MrKapowski@users.noreply.github.com>
2019-06-21 16:07:21 +0100

    extra classes

--- integrations/webmentions/templates/webmention-form.php
+++ integrations/webmentions/templates/webmention-form.php
@@ -4,8 +4,8 @@
 		<?php echo get_webmention_form_text( get_the_ID() /*phpcs:ignore*/); ?>
 	</p>
 	<form id="webmention-form" class="comment--webmention--form" action="<?php echo esc_url( get_webmention_endpoint() ); ?>" method="post">
-		<input id="webmention-source" type="url" name="source" class="" placeholder="<?php esc_attr_e( 'URL/Permalink of your article', 'webmention' ); ?>" />
-		<button id="webmention-submit" type="submit" name="submit" class=""><?php esc_html_e( 'Ping me!', 'webmention' ); ?></button>
+		<input id="webmention-source" type="url" name="source" class="comment--webmention--source" placeholder="<?php esc_attr_e( 'URL/Permalink of your article', 'webmention' ); ?>" />
+		<button id="webmention-submit" type="submit" name="submit" class="comment--webmention--submit"><?php esc_html_e( 'Ping me!', 'webmention' ); ?></button>
 		<input id="webmention-format" type="hidden" name="format" value="html" />
 		<input id="webmention-target" type="hidden" name="target" value="<?php the_permalink(); ?>" />
 	</form>

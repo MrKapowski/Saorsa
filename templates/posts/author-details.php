727d94f1277f02e04931e309826d6baf3b4a789b
Chris M <35972784+MrKapowski@users.noreply.github.com>
2019-07-11 19:57:51 +0100

    fix a Lighthouse issue, reset CSS

--- templates/posts/author-details.php
+++ templates/posts/author-details.php
@@ -10,7 +10,7 @@ if ( ! is_single() ) {
 			<a href="<?php echo esc_url( get_the_author_meta( 'user_url' ) ); ?>" rel="author" itemprop="url" class="author__name p-name u-url url name fn">
 				<?php echo esc_html( get_the_author_meta( 'display_name' ) ); ?>
 			</a>
-			<img itemprop="image" class="author__photo u-photo" src="<?php echo esc_attr( get_avatar_url( get_the_author_meta( 'ID' ) ) ); ?>">
+			<img itemprop="image" alt="Avatar for <?php echo esc_attr( get_the_author_meta( 'display_name' ) ); ?>" class="author__photo u-photo" src="<?php echo esc_attr( get_avatar_url( get_the_author_meta( 'ID' ) ) ); ?>">
 		</span>
 	</span>
 </span>
